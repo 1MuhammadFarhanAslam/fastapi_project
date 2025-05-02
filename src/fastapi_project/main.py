ea56f895022ba716af2849c1481d2fa76bc2d76a
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2025-05-02 14:43:16 +0500

    Added .Devcontainer

--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -38,7 +38,7 @@ books: list[Book] = []
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Library!"}
+    return {"message": "Welcome to Library! Today is Friday"}
 
 @app.get("/books", response_model=List[Book])
 def get_books():
