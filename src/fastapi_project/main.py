6d5fd251dc914338ea39be6b4a2c20b390314530
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2025-05-03 14:09:34 +0500

    added docker compose

--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -38,7 +38,7 @@ books: list[Book] = []
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Library API."}
+    return {"message": "Welcome to Library API. Today is a good day to read a book!"}
 
 @app.get("/books", response_model=List[Book])
 def get_books():
